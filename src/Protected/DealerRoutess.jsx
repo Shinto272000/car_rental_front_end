a1e3db8e1bb1a5e8b04715c43df497e9489b9b88
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-09-02 15:44:58 +0530

    committed commit

--- src/Protected/DealerRoutess.jsx
+++ src/Protected/DealerRoutess.jsx
@@ -7,37 +7,37 @@ import { axiosInstance } from '../Config/AxiosConfig';
 const DealerRoutess = ({children}) => {
     const navigate = useNavigate()
     useEffect(() => {
-        const authToken = localStorage.getItem('authToken')
-        console.log("authtoken is ",authToken);
+        // const authToken = localStorage.getItem('authToken')
+        // console.log("authtoken is ",authToken);
         
-        // const checkUser = async () => {
-        //     try {
-        //         const res = await axiosInstance.get(
-        //             "/api/v1/dealer/check-dealer",
-        //             {
-        //                 withCredentials: true,
-        //             },
-        //         );
+        const checkUser = async () => {
+            try {
+                const res = await axiosInstance.get(
+                    "/api/v1/dealer/check-dealer",
+                    {
+                        withCredentials: true,
+                    },
+                );
 
-        //         const data = res.data;
-        //         console.log(data);
-        //         if(authToken){
-        //             navigate("/dealer/dashbord")
-        //         }
+                const data = res.data;
+                console.log(data);
+                // if(authToken){
+                //     navigate("/dealer/dashbord")
+                // }
 
-        //         if (data.success === false) {
-        //             navigate("/dealer/signup", { replace: true });
-        //         }
-        //     } catch (error) {
-        //         console.error("Error occurred while checking dealer:", error);
-        //         navigate("/dealer/signup", { replace: true });
-        //     }
-        // };
-        // checkUser();
-        if(!authToken){
-            navigate("/")
-        }
-    }, [navigate]);
+                // if (data.success === false) {
+                //     navigate("/dealer/signup", { replace: true });
+                // }
+            } catch (error) {
+                console.error("Error occurred while checking dealer:", error);
+                navigate("/dealer/signup", { replace: true });
+            }
+        };
+        checkUser();
+        // if(!authToken){
+        //     navigate("/") 
+        // }
+    }, []);
 
     return children;
 };
